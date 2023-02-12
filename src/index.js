5b378b47b5c5ebf87c12444f97d6f4f0567c436d
sergey.danusevich <vector-56@yandex.ru>
2023-02-12 20:22:48 +0300

    feat: complete tast 7 pass and 1 fail

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 module.exports = function towelSort(matrix) {
-  let resultArr = [];
-  if (matrix === undefined) {
-    return []
-  }
-  for (i = 0; i < matrix.length; i++) {
-    if (i % 2 === 0) {
-      resultArr.push(...matrix[i])
-    } else {
+  const resultArr = []
+  for (let i=0; i<matrix.length; i++) {
+    if(i%2!==0) {
       resultArr.push(...matrix[i].reverse())
+    } else {
+    resultArr.push(...matrix[i])
     }
   }
-  return resultArr;
+  return resultArr
 }
